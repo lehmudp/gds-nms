db50257fd23c1960bb0e994738a1d0e3cb622c7d
Hieu <tranhieu3101994@gmail.com>
2020-09-17 21:15:55 +0700

    first commit

--- js/main-script.js
+++ js/main-script.js
@@ -1,27 +1,61 @@
-Vue.component('type-occurance', { 
-	template: '<div>Occurance component</div>' 
-})
-Vue.component('type-status', { 
-	template: '<div>Status component</div>' 
-})
-Vue.component('type-recovery', { 
-	template: '<div>Recovery component</div>' 
-})
-Vue.component('type-rfo', { 
-	template: '<div>RFO component</div>' 
-})
+const circuitData = { 
+    "circuit": {
+        "Acecook": [
+            {"Line":"Line Name - VNPT"},
+            {"Line":"Line Name - FPT"},
+            {"Line":"Line Name - Viettel"}
+        ],
+        "Boramtek": [
+            {"Line":"Line Name - CMC"}
+        ],
+        "Uniqlo": [
+            {"Line":"Line Name - Viettel"},
+            {"Line":"Line Name - NetNam"},
+        ]
+    }
+}
+
 
 window.onload = function () {
 	var app = new Vue({
 	  el: '#app',
 	  data: {
 	    currentType: 'Occurance',
-	    types: ['Occurance', 'Status', 'Recovery', 'RFO']
+	    types: ['Occurance', 'Status', 'Recovery', 'RFO'],
+	    circuits: circuitData.circuit,
+	    companyName: '',
+	    circuitList: [],
+	    circuitName: '',
 	  },
 	  computed: {
-	    currentTypeComponent: function () {
-	      return 'type-' + this.currentType.toLowerCase()
+	    actionText: function () {
+	    	if (this.currentType === 'Occurance') {
+	        	return 'Now Investigating'
+	    	}
+	    	else {
+	    		return "-	List task which is done\n-	List task which is doing\n-	List task which will do (if possible)"
+	    	}
+	    },
+	    reasonText: function () {
+	    	if (this.currentType === 'RFO') {
+	        	return 'Detail of reason'
+	    	}
+	    	else {
+	    		return "Under investigation"
+	    	}
+	    },
+	    recovered: function () {
+	    	return this.currentType === 'Occurance' || this.currentType === 'Status';
 	    }
-	  }
+	  },
+	  methods: {
+	  	companySelected: function () {
+	  		if (this.companyName == '') {
+	  			this.circuitName = '';
+	  			this.circuitList = [];
+	  		} 
+	  		else this.circuitList = this.circuits[this.companyName];
+	  	},
+	  },
 	})
 }
